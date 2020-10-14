2c126c532c7acf8818e647d3e80444af5936d50d
simonugor <61743750+simonugor@users.noreply.github.com>
2020-10-14 03:10:29 +0200

    Updates

---
+++ precache-manifest.9d8844f8403b7a03567210ace136f385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb585b176beeea4b6d77af3bfa1de1e",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "0c43df0a14916c1d8660",
+    "url": "/to-do-list-react/static/css/main.f409fda5.chunk.css"
+  },
+  {
+    "revision": "776256475866e8ff04a6",
+    "url": "/to-do-list-react/static/js/2.0f2108a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-react/static/js/2.0f2108a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c43df0a14916c1d8660",
+    "url": "/to-do-list-react/static/js/main.c435b4ed.chunk.js"
+  },
+  {
+    "revision": "ffdb8dcd54a7079ecbdd",
+    "url": "/to-do-list-react/static/js/runtime-main.28ed04a0.js"
+  }
+]);
